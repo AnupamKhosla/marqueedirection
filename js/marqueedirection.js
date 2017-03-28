111dfc2741fe2463f3a3adb1cd3371baf8396b5a
ANUPAM KHOSLA <anupamkhosla1993@gmail.com>
2017-03-28 19:57:00 +0530

    Fixed a lot of bugs
    Many bugs removed. Now it works on both chrome and firefox. Inbuilt revalidate on window resize.

--- js/marqueedirection.js
+++ js/marqueedirection.js
@@ -1,123 +1,175 @@
- $.fn.extend({
-     marquee: function(options) {
-         this.each(function() {
+;(function($) {
+    $.fn.extend({
+        marquee: function(options) {
 
-             var that = this; //To prevent any scoping issue with this
-             that.defaultOptions = {
-                 speed: 10, //10 px per second
-                 direction: "vertical" // Verical scrolling marquee            
-             }
-             var $settings = $.extend({}, options, that.defaultOptions, {
-                 speed: $(that).data("speed"),
-                 direction: $(that).data("direction")
-             });
+            this.each(function() {
 
-             var $forward, $backward, $pause, $revalidate; // Main feature functions              
+                var that = this; //To prevent any scoping issue with this
+                that.defaultOptions = {
+                    speed: 10, //10 px per second
+                    direction: "vertical" // Verical scrolling marquee            
+                }
+                var $settings = $.extend({}, options, that.defaultOptions, {
+                    speed: $(that).data("speed"),
+                    direction: $(that).data("direction")
+                });
 
-             var $scroll_step; // The remaining scroll distance. The distance for one animation
+                var $forward, $backward, $pause, $revalidate; // Main feature functions              
 
-             $pause = function() {
-                 $(that).stop();
-             };
+                var $scroll_step; // The remaining scroll distance. The distance for one animation
+                var $half_scroll; //To store nearest integer of two_divs_scrollHeight/2 or two_divs_scrollWidth/2 
+                var $speed; //Dynamic speed as per remaining amount of scroll
 
-             if (($settings.direction == "vertical")) {
+                $pause = function() {
+                    $(that).stop();
+                };
 
-                 $forward = function(event, settings) {
+                if (($settings.direction == "vertical")) {
 
+                    $forward = function(event, settings) {
 
-                     $(that).stop();
-                     if ($(that).scrollTop() >= $(that).prop('scrollHeight') / 2) {
-                         $(that).scrollTop($(that).scrollTop() - $(that).prop('scrollHeight') / 2);
-                     }
+                        $half_scroll = Math.round($(that).prop("scrollHeight") / 2);                        
 
-                     $scroll_step = $(that).prop('scrollHeight') / 2 - $(that).scrollTop();
+                        $(that).stop();
+                        if ($(that).scrollTop() >= $half_scroll) {                            
+                            $(that).scrollTop($(that).scrollTop() - $half_scroll);
+                        }
 
-                     $(that).animate({
-                         scrollTop: $(that).prop('scrollHeight') / 2
-                     }, (1 / (!!settings ? settings.speed : $settings.speed)) * $scroll_step * 1000, "linear", function() { $forward(event, settings) });
+                        $scroll_step = $half_scroll - $(that).scrollTop();
 
-                 };
+                        if($settings.speed > 0) {
+                            $speed = (!!settings ? settings.speed : $settings.speed);
+                        }
+                        else {
+                            console.error("speed must be > 0");
+                            $speed = 1;
+                        }                       
 
-                 $backward = function(event, settings) {
+                       
+                        $(that).animate({
+                            scrollTop: $half_scroll
+                        }, (1 / $speed) * $scroll_step * 1000, "linear", function() { $forward(event, settings) });
 
-                     $(that).stop();
-                     if ($(that).scrollTop() <= $(that).prop('scrollHeight') / 2 - $(that).outerHeight()) {
-                         $(that).scrollTop($(that).scrollTop() + $(that).prop('scrollHeight') / 2);
-                     }
+                    };
 
-                     $scroll_step = $(that).outerHeight() + $(that).scrollTop() - $(that).prop('scrollHeight') / 2;
+                    $backward = function(event, settings) {
 
-                     $(that).animate({
-                         scrollTop: $(that).prop('scrollHeight') / 2 - $(that).outerHeight()
-                     }, (1 / (!!settings ? settings.speed : $settings.speed)) * $scroll_step * 1000, "linear", function() { $backward(event, settings) });
-                 }
+                        $half_scroll = Math.round($(that).prop("scrollHeight") / 2);
 
+                        $(that).stop();
+                        if ($(that).scrollTop() <= $half_scroll - $(that).outerHeight()) {
+                            $(that).scrollTop($(that).scrollTop() + $half_scroll);
+                        }
 
-             } else if (($settings.direction == "horizontal")) {
+                        $scroll_step = $(that).outerHeight() + $(that).scrollTop() - $half_scroll;
 
-                 //$(that).find(".marquee-wrapper").width( 2*$(that).find(".marquee-content").outerWidth() );
+                        if($settings.speed > 0) {
+                            $speed = (!!settings ? settings.speed : $settings.speed);
+                        }
+                        else {
+                            console.error("speed must be > 0");
+                            $speed = 1;
+                        }
 
-
-                 $forward = function(event, settings) {
-
-
-                     $(that).stop();
-
-                     if ($(that).scrollLeft() >= ($(that).prop('scrollWidth') / 2)) {
-                         $(that).scrollLeft($(that).scrollLeft() - $(that).prop('scrollWidth') / 2);
-                     }
-
-                     $scroll_step = $(that).prop('scrollWidth') / 2 - $(that).scrollLeft();
-
-                     $(that).animate({
-                         scrollLeft: $(that).prop('scrollWidth') / 2
-                     }, (1 / (!!settings ? settings.speed : $settings.speed)) * $scroll_step * 1000, "linear", function() { $forward(event, settings) });
-
-                 };
-
-                 $backward = function(event, settings) {
-
-                     $(that).stop();
-                     if ($(that).scrollLeft() <= $(that).prop('scrollWidth') / 2 - $(that).outerHeight()) {
-                         $(that).scrollLeft($(that).scrollLeft() + $(that).prop('scrollWidth') / 2);
-                     }
-
-                     $scroll_step = $(that).outerHeight() + $(that).scrollLeft() - $(that).prop('scrollWidth') / 2;
-
-                     $(that).animate({
-                         scrollLeft: $(that).prop('scrollWidth') / 2 - $(that).outerHeight()
-                     }, (1 / (!!settings ? settings.speed : $settings.speed)) * $scroll_step * 1000, "linear", function() { $backward(event, settings) });
-
-                 }
-             }
+                        $(that).animate({
+                            scrollTop: $half_scroll - $(that).outerHeight()
+                        }, (1 / $speed) * $scroll_step * 1000, "linear", function() { $backward(event, settings) });
+                    }
 
 
-             $revalidate = function() {
+                } else if (($settings.direction == "horizontal")) {
+                 
 
-                 if (!((($settings.direction == "vertical") && ($(that).outerHeight() <= $(that).find(".marquee-content").outerHeight(true))) || (($settings.direction == "horizontal") && ($(that).width() <= $(that).find(".marquee-content").outerWidth(true))))) {
+                    $forward = function(event, settings) {
 
-                     $(that).find(".marquee-content").eq(1).remove();
-                     $(that).off("pause forward backward");
-                 } else {
-                     if (!$(that).find(".marquee-content").eq(1).length) {
-                         $(that).find(".marquee-content").clone().appendTo($(that).find(".marquee-wrapper"));
-                     }
+                        $half_scroll = Math.round($(that).prop("scrollWidth") / 2);
+                        $(that).stop();
+
+                        if ($(that).scrollLeft() >= ($half_scroll)) {
+                            $(that).scrollLeft($(that).scrollLeft() - $half_scroll);
+                        }
+
+                        $scroll_step = $half_scroll - $(that).scrollLeft();
+                          if($settings.speed > 0) {
+                            $speed = (!!settings ? settings.speed : $settings.speed);
+                        }
+                        else {
+                            console.error("speed must be > 0");
+                            $speed = 1;
+                        }
+
+                        $(that).animate({
+                            scrollLeft: $half_scroll
+                        }, (1 / $speed) * $scroll_step * 1000, "linear", function() { $forward(event, settings) });
+
+                    };
+
+                    $backward = function(event, settings) {
+
+                        $half_scroll = Math.round($(that).prop("scrollWidth") / 2);
+                        $(that).stop();
+                        if ($(that).scrollLeft() <= $half_scroll - $(that).outerHeight()) {
+                            $(that).scrollLeft($(that).scrollLeft() + $half_scroll);
+                        }
+
+                        $scroll_step = $(that).outerHeight() + $(that).scrollLeft() - $half_scroll;
+                          if($settings.speed > 0) {
+                            $speed = (!!settings ? settings.speed : $settings.speed);
+                        }
+                        else {
+                            console.error("speed must be > 0");
+                            $speed = 1;
+                        }
+
+                        $(that).animate({
+                            scrollLeft: $half_scroll - $(that).outerHeight()
+                        }, (1 / $speed) * $scroll_step * 1000, "linear", function() { $backward(event, settings) });
+
+                    }
+                }
 
 
-                     $(that).on("pause", $pause);
-                     $(that).on("forward", $forward);
-                     $(that).on("backward", $backward);
+                $revalidate = function() {
 
-                 }
-             }
+                    if (!((($settings.direction == "vertical") && ($(that).outerHeight() <= $(that).find(".marquee-content").outerHeight())) || (($settings.direction == "horizontal") && ($(that).outerWidth() <= $(that).find(".marquee-content").outerWidth())))) {
+                        $(that).trigger("pause");
+                        $(that).find(".marquee-content").eq(1).remove();
+                        $(that).off("pause forward backward");
+                    } else {
+                        if (!$(that).find(".marquee-content").eq(1).length) {
+                            $(that).find(".marquee-content").clone().appendTo($(that).find(".marquee-wrapper"));
+                        }
 
-             $(that).on("revalidate", $revalidate);
+
+                        $(that).on("pause", $pause);
+                        $(that).on("forward", $forward);
+                        $(that).on("backward", $backward);
+
+                    }
+                }
+
+                $(that).on("revalidate", $revalidate);
 
 
 
-         });
+            });
 
-        $(this).trigger("revalidate");
-         return this;
-     }
- });
+            var scope = this; //again scoping issues
+
+            function resize_revalidate(){
+                $(window).off("resize.resize_revalidate");
+                setTimeout(function(){
+                    $(scope).trigger("revalidate");                    
+                    $(window).on("resize.resize_revalidate", resize_revalidate);
+                },100);
+            }
+
+            $(this).trigger("revalidate");
+
+            $(window).on("resize.resize_revalidate", resize_revalidate);
+
+            return this;
+        }
+    });
+
+})(jQuery);
